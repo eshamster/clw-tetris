ae4a1493a3007b178d64979288d91db1ac24f948
eshamster <hamgoostar@gmail.com>
2018-01-05 08:26:24 +0000

    Update to enable restarting after gameover

--- src/game/tetris-state.lisp
+++ src/game/tetris-state.lisp
@@ -22,7 +22,12 @@
                   (init-tetris-entities)
                   (init-mouse-entity)
                   (init-score-manager)
-                  t)))))
+                  t))
+               (process
+                (lambda (_this)
+                  (declare (ignore _this))
+                  (when (gameover-p (find-a-entity-by-tag :field))
+                    (make-tetris-gameover-state)))))))
 
 (defstruct.ps+
     (tetris-start-state
@@ -52,3 +57,42 @@
                   (delete-ecs-entity (tetris-start-state-text-area-entity _this))
                   t))))
     text-area-entity)
+
+(defstruct.ps+
+    (tetris-gameover-state
+     (:include game-state
+               (start-process
+                (lambda (_this)
+                  (declare (ignore _this))
+                  (let* ((font-size 35)
+                         (margin 20)
+                         (area (make-text-area :font-size font-size :text-align :center
+                                               :margin margin
+                                               :x (/ (get-screen-width) 2)
+                                               :y (+ (/ (get-screen-height) 2)
+                                                     (+ (* font-size 2) margin)))))
+                    ;; TODO: Make key name configurable
+                    (add-text-to-area area :text "GAME OVER!!" :color #xff0000)
+                    (add-text-to-area area
+                                      :text "Press z key or Click to restart"
+                                      :color #x00ffff)
+                    (add-ecs-entity area))
+                  t))
+               (process
+                (lambda (_this)
+                  (declare (ignore _this))
+                  (when (or (is-key-down-now :a)
+                            (eq (get-left-mouse-state) :down-now))
+                    (make-tetris-main-state))))
+               (end-process
+                (lambda (_this)
+                  (with-slots (first-frame) _this
+                    (if first-frame
+                        (progn (do-ecs-entities entity
+                                 (unless (ecs-entity-parent entity)
+                                   (register-next-frame-func
+                                    (lambda () (delete-ecs-entity entity)))))
+                               (setf first-frame nil)
+                               nil)
+                        t))))))
+    (first-frame t))
